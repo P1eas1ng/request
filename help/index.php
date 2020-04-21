02c8cb6ff0fca42527f7f92242334ebdfb427232
P1eas1ng <63578449+P1eas1ng@users.noreply.github.com>
2020-04-21 20:35:44 +0300

    Update index.php

--- help/index.php
+++ help/index.php
@@ -2,4 +2,4 @@
 <p>Hello, This is an api for different needs</p>
 <p>1. To echo something, write: <a href="https://request-kama.herokuapp.com/?echo=something">https://request-kama.herokuapp.com/?echo=something</a></p>
 <p>2. To download any file from any server write: <a href="https://request-kama.herokuapp.com/?download=/file.txt">https://request-kama.herokuapp.com/?download=http://site/where/is/any/file/you/want/to/download.it</a></p>
-<p>3. To save a txt file with contents write: <a href="https://request-kama.herokuapp.com/save=somecontext">https://request-kama.herokuapp.com/save=somecontext</a></p>
+<p>3. To save a txt file with contents write: <a href="https://request-kama.herokuapp.com/?save=somecontext">https://request-kama.herokuapp.com/?save=somecontext</a></p>
