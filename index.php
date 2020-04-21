9ef05d0d069a5c88334de2a7a285f46bfa7a74a0
P1eas1ng <63578449+P1eas1ng@users.noreply.github.com>
2020-04-21 20:21:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,4 +12,16 @@ ob_end_clean();
 readfile($remoteURL);
 exit;
 }
+if(isset($_GET['save'])) {
+  $txt = $_GET['save'];
+  $myfile = fopen("file.txt", "w");
+  fwrite($myfile, $txt);
+  fclose($myfile);
+  $remoteURL = 'https://request-kama.herokuapp.com/file.txt';
+  header("Content-type: application/x-file-to-save"); 
+  header("Content-Disposition: attachment; filename=".basename($remoteURL));
+  ob_end_clean();
+  readfile($remoteURL);
+  exit;
+}
 ?>
